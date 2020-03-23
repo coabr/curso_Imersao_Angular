53b3e1a16c31fc4bf81c66895c22ab5138c55b62
COAB1 <carol.o.ag@gmail.com>
2020-03-23 15:38:04 -0300

    Inclusao do metodo incluir e do componente curso manter

--- projetoFinal/src/app/curso/curso.component.ts
+++ projetoFinal/src/app/curso/curso.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from './servico/curso';
 import { CursoService } from './servico/curso.service';
+import { Router } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-curso',
@@ -8,31 +10,30 @@ import { CursoService } from './servico/curso.service';
   styleUrls: ['./curso.component.css']
 })
 export class CursoComponent implements OnInit {
+ 
+  curso: Curso = new Curso();
+
+  listaCurso: Curso[] = [];
 
   constructor(
+    private router: Router,
     private cursoServico: CursoService
   ) { }
 
   ngOnInit(): void {
+    this.pesquisar();
   }
 
-  curso: Curso= new Curso();
-
-
-  pesquisar(){
-
+  pesquisar() {
     this.cursoServico.pesquisar(this.curso.nome).subscribe(
-      retorno => {
-        console.log(retorno);
+      (retorno: Curso[]) => {
+        this.listaCurso = retorno;
       }
     );
   }
-  
-  /* 
-  vamos usar este alerta no curso.service.ts
 
-  imprimir(){
-    alert(this.curso.nome + '. ');
+  incluir(){
+    this.router.navigate(['/curso/incluir']);
   }
-*/
+
 }
--- projetoFinal/src/app/curso/servico/curso.service.ts
+++ projetoFinal/src/app/curso/servico/curso.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Curso } from './curso';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,13 @@ export class CursoService {
   ) { }
 
 
-  pesquisar(nome){
-    return this.http.get("https://cors-anywhere.herokuapp.com/https://stormy-badlands-29216.herokuapp.com/api/curso/consultar/"+nome);
+  pesquisar(nome) {
+    return this.http.get("https://cors-anywhere.herokuapp.com/https://stormy-badlands-29216.herokuapp.com/api/curso/consultar/" + nome);
+  }
+  
+  incluir(curso: Curso) {
+    return this.http.post("https://cors-anywhere.herokuapp.com/https://stormy-badlands-29216.herokuapp.com/api/curso/incluir", curso);
   }
 
-  
+
 }
